8762a32fe2b6c4eafbea1684d1a75d53f94dd805
Nate Celeste <nceleste@kipsu.com>
2020-12-01 16:25:43 -0600

    brute force answer for 1-2

---
+++ Day1/day1-2.py
@@ -0,0 +1,9 @@
+input = open("input.txt")
+inputLines = input.read().split()
+print(inputLines[3])
+
+for i in inputLines:
+    for j in inputLines:
+        for k in inputLines:
+            if ((int(i) + int(j) + int(k)) == 2020):
+                print(int(i) * int(j) * int(k))
